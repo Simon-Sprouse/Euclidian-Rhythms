480acc33749ff12f6fd3da260cbcb84db25e0455
Simon Sprouse <simonhsprouse@gmail.com>
2024-03-29 19:01:16 -0500

    resolved module error

--- logic.js
+++ logic.js
@@ -1,12 +1,28 @@
+import Timer from "./timer.js";
+
+
 let my_button = document.getElementById("button1");
+let bpm = 120;
+const sound = new Audio("bruhShort.mp3")
 
-
-sound = new Audio("bruh.mp3")
-
+function logBruh() {
+    console.log("bruh");
+}
 
 function playSound() { 
-    console.log("Bruh");
     sound.play();
 }
 
-my_button.onclick = playSound;
+my_button.onclick = logBruh;
+
+const metronome = new Timer(playSound, 60000 / bpm, { immediate: true});
+
+metronome.start();
+
+
+
+
+
+
+
+
